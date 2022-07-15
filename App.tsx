195648e01cb247b24a91e5632c4f84027e0846a5
Oli Warriner <owarriner92@gmail.com>
2022-07-15 14:47:16 +0100

    Initial

--- App.tsx
+++ App.tsx
@@ -1,12 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableHighlight, TouchableOpacity, Image, SafeAreaView, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello World!</Text>
+      <TouchableHighlight onPress={() => console.log('Image tapped')}>
+      <Image 
+        blurRadius={5}
+        source={{
+        width: 200,
+        height: 300,
+        uri: "https://picsum.photos/200/300"}} />
+      </TouchableHighlight>
+      <Button color="orange" title='Alert Dialog' 
+      onPress={() => Alert.alert("Alert", "Some message", [
+        {text: "Cancel", onPress: () => console.log("Cancel Pressed"), style: "cancel"},
+        {text: "OK", onPress: () => console.log("OK Pressed")},
+      ])} />
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
